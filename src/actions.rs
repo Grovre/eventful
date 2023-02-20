37364e4510ca9359de238970c00c989d841f6caf
Grovre <50428844+Grovre@users.noreply.github.com>
2023-02-20 11:30:25 -0600

    remove_all_actions of a specific fn()

--- src/actions.rs
+++ src/actions.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, fmt::Display, str::FromStr};
+use std::{collections::{HashMap}, fmt::{Display}, str::FromStr};
 
 pub struct Actions {
     registered: HashMap<u128, fn()>,
@@ -26,13 +26,36 @@ impl Actions {
     pub fn remove_action(&mut self, key: &u128) -> Option<fn()> {
         self.registered.remove(key)
     }
+
+    pub fn remove_all_actions(&mut self, f: fn()) -> usize {
+        let mut count = self.registered.len();
+        let mut next_register = HashMap::new();
+        
+        for entry in &self.registered {
+            if *entry.1 == f {
+                continue
+            }
+
+            next_register.insert(*entry.0, *entry.1);
+        }
+
+        self.registered = next_register;
+        
+        count - self.registered.len()
+    }
 }
 
 impl Display for Actions {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let mut str = String::from_str("Action Keys:").unwrap();
+        let mut str = String::from_str("Key -> Action:").unwrap();
+
+        if self.registered.len() == 0 {
+            str.push_str("\nEmpty");
+            return write!(f, "{}\nEmpty", str);
+        }
+
         for entry in &self.registered {
-            let map_str = format!("\n{} -> {:p}", entry.0, entry.1);
+            let map_str = format!("\n{} -> {:p}", entry.0, *entry.1);
             str.push_str(map_str.as_str());
         }
 
